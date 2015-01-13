82430c132e3fb429b5613a744021198f497a300c
csaez <cesarte@gmail.com>
2015-01-12 23:19:43 -0300

    wip: ibl explorer mockup

--- ibl_stuff/api/__init__.py
+++ ibl_stuff/api/__init__.py
@@ -1,136 +1,135 @@
-import json
+IBL_PATH = "/home/csaez/Works/dev/ibl_stuff/refs/pano.png"
+SAMPLE_PATH = "/home/csaez/Works/dev/ibl_stuff/refs/sample.png"
 
 
-class IBL_Data(dict):
-
-    _IBL_KEYS = ("thumbnail", "render", "title", "type", "lighting",
-                 "location", "tags", "author", "date")
-
-    def __init__(self, *arg, **kwds):
-        super(IBL_Data, self).__init__(*arg, **kwds)
-        for key in self._IBL_KEYS:
-            default_value = str() if key != "tags" else list()
-            self.__setitem__(key, default_value)
-
-    def __getattr__(self, attr):
-        if attr in self._IBL_KEYS:
-            return self.__getitem__(attr)
-        return super(self, IBL_Data).__getattr__(attr)
-
-    def __setattr__(self, attr, value):
-        if attr in self._IBL_KEYS:
-            return self.__setitem__(attr, value)
-        return super(self, IBL_Data).__setattr__(attr, value)
-
-    def import_json(self, json_file):
-        with open(json_file) as fp:
-            d = json.load(fp)
-        self.update(d)
-
-    def export_json(self, json_file):
-        d = dict([(k, self.get(k)) for k in self._IBL_KEY])
-        with open(json_file, "w") as fp:
-            json.dump(d, fp)
-
-
-def _create_ibl(*args, **kwrgs):
-    ibl = IBL_Data()
-    keys = ("title", "type", "lighting", "location", "tags", "author", "date")
-    # check args
-    for i, arg in enumerate(args):
-        k = keys[i]
-        ibl[k] = arg
-    # check kwkwrgs
-    for k in keys:
-        v = kwrgs.get(k)
-        if v:
-            ibl[k] = v
-    return ibl
+def get_projects():
+    return (
+        "Iron Man 4",
+        "Fast And Furious 9",
+        "Back To The Future IV",
+        "Jurassic Park 6",
+        "The Gonnies 2",
+        "Alice In Wonderland Retrospective",
+        "Ghostbusters 5",
+        "Planet 52",
+        "Ice Age 7",
+        "Mad Max 5",
+        "The Adderal Diaries",
+    )
 
 
 def get_ibls():
-    # placeholder!
-    return (
-        _create_ibl("Sunny Boat",
-                    "Look-dev",
-                    "Sunny",
-                    "Portree, Isle of Skye, Scotland",
-                    ("exterior",
-                     "town",
-                     "sea",
-                     "natural environment",
-                     "docks",
-                     "sunny",
-                     "winter"),
-                    "Xuan Prada",
-                    "12-Feb-2014"),
-        _create_ibl("Darwing Room",
-                    "Look-Dev",
-                    "Cloudy",
-                    "Mieres, Asturias, Spain",
-                    ("interior",
-                     "industrial",
-                     "destroyed",
-                     "sunny",
-                     "shade",
-                     ),
-                    "Xuan Prada",
-                    "09-Mar-2014"),
-        _create_ibl("Pinewood Studios, Stage 02",
-                    "Texture checker",
-                    "Neutral",
-                    "Pinewood Studios, London, UK",
-                    ("interior",
-                     "artificial",
-                     "on-set",
-                     "studio",
-                     ),
-                    "Xuan Prada",
-                    "21-Apr-2014"),
-        _create_ibl("Millennium Bridge",
-                    "Look-Dev",
-                    "Cloudy",
-                    "City of Westminster, London, UK",
-                    ("exterior",
-                     "urban",
-                     "cloudy",
-                     "stormy",
-                     ),
-                    "Xuan Prada",
-                    "17-May-2014"),
-        _create_ibl("The Old Man of Storr",
-                    "Look-dev",
-                    "Cloudy",
-                    "Portree, Isle of Skye, Scotland",
-                    ("exterior",
-                     "natural",
-                     "rocks",
-                     "cloudy",
-                     "stormy",
-                     ),
-                    "Xuan Prada",
-                    "24-Jun-2014"),
-        _create_ibl("Studio C",
-                    "Model checker",
-                    "Neutral",
-                    "Computer generated",
-                    ("interior",
-                     "artificial",
-                     "studio",
-                     "cg generated",
-                     ),
-                    "Xuan Prada",
-                    "04-Jul-2014"),
-        _create_ibl("Mercury Mine",
-                    "Look-dev",
-                    "Sunny",
-                    "Mieres, Asturis, Spain",
-                    ("interior",
-                     "industrial",
-                     "destroyed",
-                     "sunny",
-                     "shade",
-                     ),
-                    "Xuan Prada",
-                    "17-Aug-2014"),
-    )
+    return ({"title": "Sunny Boat",
+             "type": "Look-dev",
+             "lighting": "Sunny",
+             "location": "Portree, Isle of Skye, Scotland",
+             "tags": ("exterior",
+                      "town",
+                      "sea",
+                      "natural environment",
+                      "docks",
+                      "sunny",
+                      "winter"),
+             "author": "Xuan Prada",
+             "date": "12-Feb-2014",
+             "comments": "This IBL was shot for the coat crash sequence.",
+             "pano": IBL_PATH,
+             "sample": SAMPLE_PATH,
+             },
+            {"title": "Darwing Room",
+             "type": "Look-Dev",
+             "lighting": "Cloudy",
+             "location": "Mieres, Asturias, Spain",
+             "tags": ("interior",
+                      "industrial",
+                      "destroyed",
+                      "sunny",
+                      "shade",
+                      ),
+             "author": "Xuan Prada",
+             "date": "09-Mar-2014",
+             "comments": "To be used for digital doubles in pt_mc_04 seq.",
+             "pano": IBL_PATH,
+             "sample": SAMPLE_PATH,
+             },
+            {"title": "Pinewood Studios, Stage 02",
+             "type": "Texture checker",
+             "lighting": "Neutral",
+             "location": "Pinewood Studios, London, UK",
+             "tags": ("interior",
+                      "artificial",
+                      "on-set",
+                      "studio",
+                      ),
+             "author": "Xuan Prada",
+             "date": "21-Apr-2014",
+             "comments": "Delorean textures need to be approved using this.",
+             "pano": IBL_PATH,
+             "sample": SAMPLE_PATH,
+             },
+            {"title": "Millennium Bridge",
+             "type": "Look-Dev",
+             "lighting": "Cloudy",
+             "location": "City of Westminster, London, UK",
+             "tags": ("exterior",
+                      "urban",
+                      "cloudy",
+                      "stormy",
+                      ),
+             "author": "Xuan Prada",
+             "date": "17-May-2014",
+             "comments": "This IBL is for look-deving the digital building " +
+                         "for the opening sequence.\n" +
+                         "Important: Forgot to shoot Macbeth charts, use " +
+                         "grey ball for color grading.",
+             "pano": IBL_PATH,
+             "sample": SAMPLE_PATH,
+             },
+            {"title": "The Old Man of Storr",
+             "type": "Look-dev",
+             "lighting": "Cloudy",
+             "location": "Portree, Isle of Skye, Scotland",
+             "tags": ("exterior",
+                      "natural",
+                      "rocks",
+                      "cloudy",
+                      "stormy",
+                      ),
+             "author": "Xuan Prada",
+             "date": "24-Jun-2014",
+             "comments": "To be used for CG cars in jm_pr_02 sequence.",
+             "pano": IBL_PATH,
+             "sample": SAMPLE_PATH,
+             },
+            {"title": "Studio C",
+             "type": "Model checker",
+             "lighting": "Neutral",
+             "location": "Computer generated",
+             "tags": ("interior",
+                      "artificial",
+                      "studio",
+                      "cg generated",
+                      ),
+             "author": "Xuan Prada",
+             "date": "04-Jul-2014",
+             "comments": "CG models need to be approved using this IBL.",
+             "pano": IBL_PATH,
+             "sample": SAMPLE_PATH,
+             },
+            {"title": "Mercury Mine",
+             "type": "Look-dev",
+             "lighting": "Sunny",
+             "location": "Mieres, Asturis, Spain",
+             "tags": ("interior",
+                      "industrial",
+                      "destroyed",
+                      "sunny",
+                      "shade",
+                      ),
+             "author": "Xuan Prada",
+             "date": "17-Aug-2014",
+             "comments": "To be used for look-deving digital dogs",
+             "pano": IBL_PATH,
+             "sample":  SAMPLE_PATH,
+             },
+            )
--- ibl_stuff/gui/explorer.py
+++ ibl_stuff/gui/explorer.py
@@ -1,55 +1,112 @@
 from PySide import QtGui
-from ibl_stuff.api import get_ibls
+from ibl_stuff import api
+
+
+class Preview(QtGui.QGraphicsView):
+
+    def __init__(self, *arg, **kwds):
+        super(Preview, self).__init__(*arg, **kwds)
+        self.setScene(QtGui.QGraphicsScene())
+        self.setFixedSize(250, 125)
 
 
 class Card(QtGui.QWidget):
 
     labels = ("title", "type", "lighting", "comments")
+    title = property(lambda s: s._fget("title"),
+                     lambda s, x: s._fset("title", x))
+    type = property(lambda s: s._fget("type"),
+                    lambda s, x: s._fset("type", x))
+    lighting = property(lambda s: s._fget("lighting"),
+                        lambda s, x: s._fset("lighting", x))
+    comments = property(lambda s: s._fget("comments"),
+                        lambda s, x: s._fset("comments", x))
 
-    def __init__(self, ibl, *arg, **kwds):
-        super(Card, self).__init__(*arg, **kwds)
-        self.ibl_data = ibl
+    def __init__(self, *arg, **kwds):
+        super(Card, self).__init__()
+        # init data
+        for k, v in kwds.iteritems():
+            if k in self.labels:
+                setattr(self, "_" + k, v)
+        # init ui
         self.init_ui()
 
-    def init_ui(self):
-        # init labels
-        for each in self.labels:
-            setattr(self, each, QtGui.QLabel())
+    def _fget(self, arg):
+        if hasattr(self, "_" + arg):
+            return getattr(self, "_" + arg)
+        return ""
 
-        # set layout
-        hbox = QtGui.QHBoxLayout()
-        for x in (self.lighting, QtGui.QLabel("-"), self.type):
-            hbox.addWidget(x)
-        hbox.addStretch(1)
-        vbox = QtGui.QVBoxLayout()
-        vbox.addWidget(self.title)
-        vbox.addLayout(hbox)
-        vbox.addWidget(self.comments)
-        self.setLayout(vbox)
-
+    def _fset(self, arg, value):
+        setattr(self, arg, value)
         self.update()
 
-    def update(self, ibl_data=None):
-        if ibl_data is not None:
-            self.ibl_data = ibl_data
-        # update labels
+    def init_ui(self):
+        self.ui_thumb = Preview()
+        for each in self.labels:
+            setattr(self, "ui_" + each, QtGui.QLabel())
+        f = self.ui_title.font()
+        f.setPointSize(f.pointSize() * 1.5)
+        f.setBold(True)
+        f.setCapitalization(QtGui.QFont.AllUppercase)
+        self.ui_title.setFont(f)
+        # set layout
+        label_hbox = QtGui.QHBoxLayout()
+        for x in (self.ui_lighting, QtGui.QLabel("-"), self.ui_type):
+            label_hbox.addWidget(x)
+        label_hbox.addStretch(1)
+        label_vbox = QtGui.QVBoxLayout()
+        label_vbox.addWidget(self.ui_title)
+        label_vbox.addLayout(label_hbox)
+        label_vbox.addWidget(self.ui_comments)
+        label_vbox.addStretch(1)
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.addWidget(self.ui_thumb)
+        hbox.addLayout(label_vbox)
+
+        self.setLayout(hbox)
+        self.update()
+
+    def update(self):
         for x in self.labels:
-            data = self.ibl_data.get(x) if x != "tags" else ",".join(x)
-            getattr(self, x).setText(data)
+            data = self._fget(x)
+            getattr(self, "ui_" + x).setText(data)
 
 
 class Explorer(QtGui.QMainWindow):
 
     def __init__(self, *arg, **kwds):
         super(Explorer, self).__init__(*arg, **kwds)
-        title = type(self).__name__.replace("_", " ")
+        title = "IBL Stuff - " + type(self).__name__.replace("_", " ")
         self.setWindowTitle(title)
         self.init_ui()
 
+    def add_ibl(self, **kwds):
+        item = QtGui.QListWidgetItem()
+        c = Card(**kwds)
+        item.setSizeHint(c.sizeHint())
+        self.ibl_list.addItem(item)
+        self.ibl_list.setItemWidget(item, c)
+        return c
+
     def init_ui(self):
-        layout = QtGui.QVBoxLayout()
-        for ibl in get_ibls():
-            layout.addWidget(Card(ibl))
+        # set IBLs
+        self.ibl_list = QtGui.QListWidget()
+        for ibl in api.get_ibls():
+            self.add_ibl(title=ibl.get("title"),
+                         type=ibl.get("type"),
+                         lighting=ibl.get("lighting"),
+                         comments=ibl.get("comments"))
+        # set projects
+        self.project_list = QtGui.QListWidget()
+        for p in api.get_projects():
+            self.project_list.addItem(QtGui.QListWidgetItem(p))
+        self.project_list.setMaximumWidth(180)
+        # layout
+        hbox = QtGui.QHBoxLayout()
+        hbox.addWidget(self.ibl_list)
+        hbox.addWidget(self.project_list)
         w = QtGui.QWidget()
-        w.setLayout(layout)
+        w.setLayout(hbox)
         self.setCentralWidget(w)
+        self.resize(1024, 550)
